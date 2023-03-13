4c800775d3f8c063c12d9156027dbaa964e727aa
mayuresh patil <mayuresh.patil@ibm.com>
2023-03-13 16:05:34 +0530

    i have added

--- src/test_cases.java
+++ src/test_cases.java
@@ -4,11 +4,11 @@ public class test_cases {
 
     @Test
     void steup(){
-        System.out.println(" i am inside setup");
+        System.out.println(" i am inside setup adadad sfaddsf");
     }
     @Test
     void testSteps(){
-        System.out.println("I am inside test step");
+        System.out.println("I am inside test step adadad");
     }
     @Test
     void tearDown(){
